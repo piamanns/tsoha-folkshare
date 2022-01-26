b7c3e46d6aa92fe0abd8ae3f834e811032568838
Pia Manns <pia.manns@gmail.com>
2022-01-26 20:29:51 +0200

    Add separate page for displaying individual tunes

--- routes.py
+++ routes.py
@@ -4,7 +4,14 @@ from db import db
 
 @app.route("/")
 def index():
-    sql = "SELECT * FROM songs ORDER BY id DESC"
+    sql = "SELECT id, name, created FROM tunes ORDER BY id DESC"
     result = db.session.execute(sql)
-    song = result.fetchone()
-    return render_template("index.html", song=song)
+    tunes = result.fetchall()
+    return render_template("index.html", tunes=tunes)
+
+@app.route("/tune/<int:id>")
+def tune(id):
+    sql = "SELECT id, name, notation, created FROM tunes WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    tune = result.fetchone()
+    return render_template("tune.html", tune=tune)
