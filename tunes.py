ef234d840987922e0168df826ea53710ad2e5f91
Pia Manns <pia.manns@gmail.com>
2022-02-04 20:59:02 +0200

    Fix SQL syntax

--- tunes.py
+++ tunes.py
@@ -10,13 +10,13 @@ def get_tune(id):
 
 def add_tune(name, notation, categories, user_id):
     try:  
-        sql = "INSERT INTO tunes (name, notation, created, creator_id, visible) VALUES (:name, \
-        :notation, NOW(), :user_id, TRUE) RETURNING id"
+        sql = "INSERT INTO tunes (name, notation, created, creator_id, visible) VALUES (:name, " \
+              ":notation, NOW(), :user_id, TRUE) RETURNING id"
         result = db.session.execute(sql, {"name": name, "notation": notation, "user_id": user_id})
         tune_id = result.fetchone()[0]
         for category in categories:
-            sql = "INSERT INTO categories_tunes (category_id, tune_id) VALUES (:category_id, \
-            :tune_id)"
+            sql = "INSERT INTO categories_tunes (category_id, tune_id) VALUES (:category_id, " \
+                  ":tune_id)"
             db.session.execute(sql, {"category_id": category, "tune_id": tune_id})
         db.session.commit()
         return tune_id
@@ -24,12 +24,12 @@ def add_tune(name, notation, categories, user_id):
         return False
 
 def get_all_categories():
-    sql = 'SELECT id, name FROM categories WHERE visible=TRUE'
+    sql = "SELECT id, name FROM categories WHERE visible=TRUE"
     result = db.session.execute(sql)
     return result.fetchall()
 
 def get_tune_categories(tune_id):
-    sql = 'SELECT c.name FROM categories c, categories_tunes ct WHERE ct.tune_id=:tune_id \
-          AND c.id=ct.category_id AND c.visible=TRUE'
+    sql = "SELECT c.name FROM categories c, categories_tunes ct WHERE ct.tune_id=:tune_id " \
+          "AND c.id=ct.category_id AND c.visible=TRUE"
     result = db.session.execute(sql, {"tune_id": tune_id})
     return result.fetchall()
