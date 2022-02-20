a0fc94660b74cb4d2455f8f9118a95674fcc9ed6
Pia Manns <pia.manns@gmail.com>
2022-02-20 13:01:17 +0200

    Fix bug relating to updating tune
    Saving modifications to tune with no categories threw an error. Removing all categories from a tune also works now.

--- tunes.py
+++ tunes.py
@@ -29,12 +29,16 @@ def add_tune(name, notation, categories, user_id):
 def update_tune(id, name, notation, categories):
     sql = "UPDATE tunes SET name=:name, notation=:notation, updated=NOW() WHERE id=:id"
     db.session.execute(sql, {"id": id, "name": name, "notation": notation})
-    cat_list = tuple(categories)
-    sql = "DELETE FROM categories_tunes WHERE tune_id=:tune_id AND category_id NOT IN :list"
-    db.session.execute(sql, {"tune_id": id, "list": cat_list})
-    sql = "INSERT INTO categories_tunes (category_id, tune_id) SELECT c.id, t.id FROM categories c, tunes t " \
-          "WHERE c.id IN :list AND t.id=:tune_id ON CONFLICT (category_id, tune_id) DO NOTHING"
-    db.session.execute(sql, {"tune_id": id, "list": cat_list})
+    if categories:
+        cat_list = tuple(categories)
+        sql = "DELETE FROM categories_tunes WHERE tune_id=:tune_id AND category_id NOT IN :list"
+        db.session.execute(sql, {"tune_id": id, "list": cat_list})
+        sql = "INSERT INTO categories_tunes (category_id, tune_id) SELECT c.id, t.id FROM categories c, tunes t " \
+              "WHERE c.id IN :list AND t.id=:tune_id ON CONFLICT (category_id, tune_id) DO NOTHING"
+        db.session.execute(sql, {"tune_id": id, "list": cat_list})
+    else:
+        sql = "DELETE FROM categories_tunes WHERE tune_id=:tune_id"
+        db.session.execute(sql, {"tune_id": id})
     db.session.commit()
 
 def delete_tune(id):
